efb6419dd5fcc8d63070bc9ece1b08ea3b3a7ef7
Jack <flapjack.mail@gmail.com>
2019-04-24 05:19:38 -0600

    Sprites and Player Movement
    Added a few more sprite sheets and implemented some basic 2d character movement.

---
+++ SideScroller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    
+    private Rigidbody2D Playerrigidbody;
+
+    void Start()
+    {
+        Playerrigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Playerrigidbody.velocity = new Vector2(moveSpeed, Playerrigidbody.velocity.y);
+	
+	if(Input.GetKeyDown(KeyCode.Space))
+	{
+	     Playerrigidbody.velocity = new Vector2(Playerrigidbody.velocity.x, jumpForce);
+	}
+    }
+}
